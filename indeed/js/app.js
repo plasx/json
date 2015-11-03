6d6d218cc5ce8ffcc2dca90686efa3629d024b2e
plasx <plasx@live.com>
2015-11-02 23:50:45 -0500

    rough draft of indeed api grabbing jobs for java in nyc, must add way more functionality and better ui/expierence

--- indeed/js/app.js
+++ indeed/js/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-	$.getJSON('http://api.indeed.com/ads/apisearch?publisher=3815722920288640&q=java&l=austin%2C+tx&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json', function(data){
-		console.log(data);
+	$.getJSON("http://api.indeed.com/ads/apisearch?publisher=3815722920288640&q=java&l=New%20York%2C+NY&sort=&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&as_cmp=APX+LABS&co=us&v=2&format=json&callback=?", function(data){
+		myData = data.results;
+		console.log(myData);
+		var html = "";
+		for(var i = 0, len = myData.length;i < len; i++ ){
+			console.log(myData[i].company);
+			html += '<p>' + myData[i].company + '</p>';
+		}
+		$('#titleJob1').html(html);
 	});
 });
